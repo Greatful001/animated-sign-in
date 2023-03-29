76df9c374c53ff8c20af73ab3b8fc9937d5f4ff4
Greatful001 <111737576+Greatful001@users.noreply.github.com>
2023-03-29 02:31:27 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+let log = document.getElementById("login");
+let Reg = document.getElementById("register");
+let Bt = document.getElementById("btn");
+
+function Register() {
+    log.style.left = "-450px";
+    Reg.style.left = "50px";
+    Bt.style.left = "110px";
+   
+}
+
+function login() {
+    log.style.left = "50px";
+    Reg.style.left = "450px";
+    Bt.style.left = "0px";
+   
+}
